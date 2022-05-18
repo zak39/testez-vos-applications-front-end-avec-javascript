34d1cf1040728e83cf5bf63ee16f8569df4340f4
Baptiste Fotia <fotia.baptiste@hotmail.com>
2022-05-18 17:50:12 +0200

    test(JS): Correction de mon exercice

--- js/utils/signInForm/index.test.js
+++ js/utils/signInForm/index.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import {
+ import {
     screen,
     getByTestId,
     getByLabelText,
@@ -11,17 +11,17 @@ import {
 import { router } from '../../router'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
+import SignInPage from '../../pages/signIn'
+import { handleSignInForm } from './index'
 
 let user
 
-beforeEach(async () => {
+beforeEach(() => {
     user = userEvent
 
-    document.body.innerHTML = `
-        <div id="root"></div>
-    `
-
-    await router()
+    document.body.innerHTML = SignInPage.render()
+    // C'est cette fonction que j'ai oublié
+    handleSignInForm()
 })
 
 afterEach(() => {
@@ -30,44 +30,46 @@ afterEach(() => {
     document.body.innerHTML = ''
 })
 
-describe('Sign In Form Integration Test Suites', () => {
-    it('Should render an error message with a bad email address', () => {
-
+describe('SignInForm Integration Test Suites', () => {
+    it('should display the error message when the e-mail is not correct', () => {
         // Je selectionne l'input associe au label
-        const input = screen.getByLabelText('Votre addresse e-mail', { selector: 'input'})
+        const input = getByLabelText(document.body, 'Votre addresse e-mail')
+
 
         // Je saisie le texte "thomas@thomas.com"
+        user.type(input, 'thomas@thomas.com' )
+
+        // Je clique sur le bouton submit
+        user.click(getByRole(document.body, 'button'))
+
+        // Je teste si le message d'erreur est bien present
+        expect(getByTestId(document.body, 'user-email-error-msg')).not.toHaveAttribute('hidden')
+    })
+
+    it('Should not display an error message when the email address is correct but it should display the password error message', () => {
+
+        // Je selectionne l'input associe au label
+        const input = getByLabelText(document.body, 'Votre addresse e-mail')
+
+        // Je saisie le texte "thomas@facadia.com"
         user.type(input, 'thomas@facadia.com')
 
         // Je clique sur le bouton submit
-        user.click(screen.getByRole('button'))
+        user.click(getByRole(document.body, 'button'))
+
+        // Je teste si le message d'erreur n'est pas present
+        expect(screen.getByTestId('user-email-error-msg')).toHaveClass('hidden')
 
         // Je teste si le message d'erreur est bien present
-        expect(screen.getByTestId('user-email-error-msg')).toBeVisible()
+        expect(screen.getByTestId('user-password-error-msg')).not.toHaveClass('hidden')
         
     })
 
-    it('Should render an error message with a good email address', () => {
+    it('Should display an error message when the password is not correct', () => {
 
         // Je selectionne l'input associe au label
-        const input = screen.getByLabelText('Votre addresse e-mail', { selector: 'input'})
-
-        // Je saisie le texte "thomas@thomas.com"
-        user.type(input, 'thomas@facadia.com')
-
-        // Je clique sur le bouton submit
-        user.click(screen.getByRole('button'))
-
-        // Je teste si le message d'erreur est bien present
-        expect(screen.getByTestId('user-password-error-msg')).toBeVisible()
-        
-    })
-
-    it('Should render an error message with a good email address and a bad password', () => {
-
-        // Je selectionne l'input associe au label
-        const inputEmail = screen.getByLabelText('Votre addresse e-mail', { selector: 'input'})
-        const inputPassword = screen.getByLabelText('Votre mot de passe', { selector: 'input'})
+        const inputEmail = getByLabelText(document.body, 'Votre addresse e-mail')
+        const inputPassword = getByLabelText(document.body, 'Votre mot de passe')
 
         // Je saisie le texte "thomas@thomas.com"
         user.type(inputEmail, 'thomas@facadia.com')
@@ -75,28 +77,30 @@ describe('Sign In Form Integration Test Suites', () => {
         user.type(inputPassword, 'azeaze')
 
         // Je clique sur le bouton submit
-        user.click(screen.getByRole('button'))
+        user.click(getByRole(document.body, 'button'))
 
         // Je teste si le message d'erreur est bien present
-        expect(screen.getByTestId('user-password-error-msg')).toBeVisible()
+        expect(getByTestId(document.body, 'user-password-error-msg')).not.toHaveClass('hidden')
         
     })
 
-    it('Should not render an error message with a good email address and password', () => {
+    it('Should not display any error messages since both email and password are correct', () => {
 
         // Je selectionne l'input associe au label
-        const inputEmail = screen.getByLabelText('Votre addresse e-mail', { selector: 'input'})
-        const inputPassword = screen.getByLabelText('Votre mot de passe', { selector: 'input'})
+        const inputEmail = getByLabelText(document.body, 'Votre addresse e-mail')
+        const inputPassword = getByLabelText(document.body, 'Votre mot de passe')
 
         // Je saisie le texte "thomas@thomas.com"
         user.type(inputEmail, 'thomas@facadia.com')
         user.type(inputPassword, 'azerty')
 
         // Je clique sur le bouton submit
-        user.click(screen.getByRole('button'))
+        user.click(getByRole(document.body, 'button'))
 
-        // Je teste si le message d'erreur est bien present
-        expect(screen.getByTestId('user-password-error-msg')).not.toBeVisible()
+        // Je teste si le message d'erreur n'est pas present
+        expect(getByTestId(document.body, 'user-email-error-msg')).toHaveClass('hidden')
+        // Je teste si le message d'erreur n'est pas present
+        expect(getByTestId(document.body, 'user-password-error-msg')).toHaveClass('hidden')
         
     })
 })
