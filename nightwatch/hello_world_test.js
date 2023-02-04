dd2deef3a0502391ad2e1db62f017f870749d0d5
zak39 <fotia.baptiste@hotmail.com>
2023-02-04 16:56:50 +0100

    test(e2e): Correction du test e2e de base

--- nightwatch/hello_world_test.js
+++ nightwatch/hello_world_test.js
@@ -1,3 +1,4 @@
+
 module.exports = {
     test: client => {
         client
@@ -9,10 +10,10 @@ module.exports = {
             .click('input[type=submit')
             .waitForElementVisible('.results--main')
             .assert.visible('#r1-0')
-            .assert.visible('#r1-0 .result__a')
-            .assert.containsText('#r1-0 .result__a', 'Hello world — Wikipédia')
-            .click('#r1-0 .result__a')
+            .assert.visible('#r1-0 .ikg2IXiCD14iVX7AdZo1 .LnpumSThxEWMIsDdAT17 .eVNpHGjtxRBq_gLOfGDr .EKtkFWMYpwzMKOYr0GYm')
+            .assert.containsText('#r1-0 .ikg2IXiCD14iVX7AdZo1 .LnpumSThxEWMIsDdAT17 .eVNpHGjtxRBq_gLOfGDr .EKtkFWMYpwzMKOYr0GYm', '"Hello, World!" program - Wikipedia')
+            .click('#r1-0 .ikg2IXiCD14iVX7AdZo1 .LnpumSThxEWMIsDdAT17 .eVNpHGjtxRBq_gLOfGDr .EKtkFWMYpwzMKOYr0GYm')
             .assert.visible('#firstHeading')
-            .assert.containsText('#firstHeading', 'Hello world')
+            .assert.containsText('#firstHeading span', '"Hello, World!" program')
     }
 }
